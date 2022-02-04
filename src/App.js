e87900180b7c8ba07968dbe2284bd5a90516cf02
Moussa SOW <moussa4.sow@gmail.com>
2022-02-04 01:15:07 +0100

    Add Modal

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Backdrop from './components/BackDrop';
+import Modal from './components/Modal';
 import Todo from './components/Todo'
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
       <Todo text="Learn React"/>
       <Todo text="Master React"/>
       <Todo text="Begin NextJs"/>
+      <Modal />
+      <Backdrop />
     </div>
   );
 }
